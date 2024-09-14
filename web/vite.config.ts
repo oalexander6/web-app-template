2a36842ce7bd7eadba4b0d79471b9a4e9c489ffd
Owen Alexander <owen6alexander@gmail.com>
2024-09-14 12:39:23 -0400

    Update client app dependencies

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { config as dotenvConfig } from 'dotenv';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 dotenvConfig({
-    path: '../.env'
+  path: '../.env'
 });
 
 const serverPort = process.env.PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: `http://localhost:${serverPort}/`,
-                changeOrigin: true
-            }
-        }
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${serverPort}/`,
+        changeOrigin: true
+      }
     }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
