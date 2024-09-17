16bcfacc071b45ae1ea4615f88350e50ecd0a539
Owen Alexander <owen6alexander@gmail.com>
2024-09-16 20:19:55 -0400

    Update navbar

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { CircleUser, Menu, Package2, Search } from 'lucide-react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -12,8 +12,27 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { twMerge } from 'tailwind-merge';
+import { ThemeToggle } from './theme-toggle';
+
+const navLinks = [
+  {
+    title: 'Dashboard',
+    link: '/'
+  },
+  {
+    title: 'Orders',
+    link: '#'
+  },
+  {
+    title: 'Products',
+    link: '#'
+  }
+];
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
@@ -25,36 +44,14 @@ export default function Navbar() {
             <Package2 className="h-6 w-6" />
             <span className="sr-only">Acme Inc</span>
           </a>
-          <a
-            href="#"
-            className="text-foreground transition-colors hover:text-foreground"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Orders
-          </a>
-          <a
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Products
-          </a>
-          <a
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Customers
-          </a>
-          <a
-            href="#"
-            className="text-muted-foreground transition-colors hover:text-foreground"
-          >
-            Analytics
-          </a>
+          {navLinks.map(page => 
+            <a
+              href={page.link}
+              className={twMerge("text-foreground transition-colors hover:text-foreground", location.pathname !== page.link ? "text-muted-foreground" : "")}
+            >
+              {page.title} 
+            </a>
+          )}
         </nav>
         <Sheet>
           <SheetTrigger asChild>
@@ -76,50 +73,29 @@ export default function Navbar() {
                 <Package2 className="h-6 w-6" />
                 <span className="sr-only">Acme Inc</span>
               </a>
-              <a href="#" className="hover:text-foreground">
-                Dashboard
-              </a>
-              <a
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Orders
-              </a>
-              <a
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Products
-              </a>
-              <a
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Customers
-              </a>
-              <a
-                href="#"
-                className="text-muted-foreground hover:text-foreground"
-              >
-                Analytics
-              </a>
+              {navLinks.map(page =>
+                <a href={page.link} className={twMerge("hover:text-foreground", location.pathname !== page.link ? "text-muted-foreground" : "")}>
+                  {page.title} 
+                </a>
+              )}
             </nav>
           </SheetContent>
         </Sheet>
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <div className="flex w-full items-center gap-2 md:ml-auto lg:gap-3">
           <form className="ml-auto flex-1 sm:flex-initial">
             <div className="relative">
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
               <Input
                 type="search"
-                placeholder="Search products..."
+                placeholder="Search items..."
                 className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
               />
             </div>
           </form>
+          <ThemeToggle />
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="secondary" size="icon" className="rounded-full">
+              <Button variant="outline" size="icon" className="rounded-full">
                 <CircleUser className="h-5 w-5" />
                 <span className="sr-only">Toggle user menu</span>
               </Button>
@@ -128,9 +104,7 @@ export default function Navbar() {
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem className="text-red-600 dark:text-red-400">Logout</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
